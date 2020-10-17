04f58e77092de14d092275e7899ae962dc176a24
Scott Mounce <scottmounce10@gmail.com>
2020-10-17 14:23:01 -0700

    connected to database, and builds a keyspace if it doesn't exist

--- DB/seed.js
+++ DB/seed.js
@@ -1,11 +1,21 @@
 const client = require('./index.js');
 var concat = require('concat-files');
 
-const keyspaceStart = 
+const keyspaceCarousel = 
 `CREATE KEYSPACE IF NOT EXISTS Carousel
 with replication = {
   'class': 'SimpleStrategy',
   'replication_factor': 1
 }`;
 
-client.connect().then(() => {console.log('connected to database :')}).catch((error) => {console.log(error)});
+client.connect()
+.then(() => {
+  console.log('connected to database :')  
+})
+.then(() => {
+  client.execute(keyspaceCarousel)
+})
+.then(() => {
+  client.execute('use Carousel')
+})
+.catch((error) => {console.log(error)});
