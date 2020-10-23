5aef947039490416d6dae27fb1662ff3b0756053
Scott Mounce <scottmounce10@gmail.com>
2020-10-22 17:04:11 -0700

    stripped out cassandra and added postgreSQL

--- DB/image.js
+++ DB/image.js
@@ -1,1 +1,30 @@
+const client = require('./index.js');
+const { performance } = require('perf_hooks');
 
+let loadCsv1 = `COPY carousel.imagecarousel(imageid, alt, color, imagename, product, relatedids, url) FROM '${__dirname}/../data/testData1.csv' DELIMITER ',' CSV HEADER`;
+let loadCsv2 = `COPY carousel.imagecarousel(imageid, alt, color, imagename, product, relatedids, url) FROM '${__dirname}/../data/testData2.csv' DELIMITER ',' CSV HEADER`;
+let loadCsv3 = `COPY carousel.imagecarousel(imageid, alt, color, imagename, product, relatedids, url) FROM '${__dirname}/../data/testData3.csv' DELIMITER ',' CSV HEADER`;
+let loadCsv4 = `COPY carousel.imagecarousel(imageid, alt, color, imagename, product, relatedids, url) FROM '${__dirname}/../data/testData4.csv' DELIMITER ',' CSV HEADER`;
+let loadCsv5 = `COPY carousel.imagecarousel(imageid, alt, color, imagename, product, relatedids, url) FROM '${__dirname}/../data/testData5.csv' DELIMITER ',' CSV HEADER`;
+
+let time1 = performance.now()
+client.query(loadCsv1)
+.then(() => {
+  client.query(loadCsv2)
+  .then(() => {
+    client.query(loadCsv3)
+    .then(() => {
+      client.query(loadCsv4)
+      .then(() => {
+        client.query(loadCsv5)
+        .then(() => {
+          let time2 = performance.now();
+          console.log(`it took ${time2 - time1} milliseconds to load all data`);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      })
+    })
+  })
+})
--- DB/index.js
+++ DB/index.js
@@ -1,19 +1,10 @@
-//BELOW FOR CASSANDRA 
-
-// const cassandra = require('cassandra-driver');
-
-// const client = new cassandra.Client({ 
-//   contactPoints:['127.0.0.1:9042'], 
-//   credentials: {username: 'cassandra', password: 'cassandra'},
-//   localDataCenter: 'datacenter1'
-// });
-
-//BELOW FOR POSTGRESQL
-
 const { Pool, Client } = require('pg');
 const client = new Client();
 
-
+client.connect()
+.then(() => {
+  console.log('connected to database...')
+})
 
 
 module.exports = client;
